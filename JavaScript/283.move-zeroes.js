62c766d730f600449b9bbc1e03e91b03e45411f5
nileshbohra <nmbohra2001@gmail.com>
2022-05-22 11:28:45 +0530

    using splice

--- JavaScript/283.move-zeroes.js
+++ JavaScript/283.move-zeroes.js
@@ -1,16 +1,25 @@
 const moveZeroes = (nums) => {
     // time = O(n^2), Space = O(1)
-    for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === 0) {
-            for (let j = i + 1; j < nums.length; j++) {
-                if (nums[j] != 0) {
-                    let temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    break;
-                }
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     if (nums[i] === 0) {
+    //         for (let j = i + 1; j < nums.length; j++) {
+    //             if (nums[j] != 0) {
+    //                 let temp = nums[i];
+    //                 nums[i] = nums[j];
+    //                 nums[j] = temp;
+    //                 break;
+    //             }
 
-            }
+    //         }
+    //     }
+    // }
+    let pushZeroes = 0;
+    for (let i = 0; i < nums.length - pushZeroes; i++) {
+        if (nums[i] === 0) {
+            nums.splice(i, 1);
+            nums.push(0);
+            pushZeroes++;
+            i--;
         }
     }
     return nums;
